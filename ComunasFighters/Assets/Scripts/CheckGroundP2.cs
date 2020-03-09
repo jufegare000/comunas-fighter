9d420de5c71ea27627425a576e2ddd3f09513d54
Juan Gallo <juanfgallo@experimentality.co>
2020-03-08 21:00:22 -0500

    added

---
+++ ComunasFighters/Assets/Scripts/CheckGroundP2.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckGroundP2 : MonoBehaviour
+{
+    private Playe2D2 player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GetComponentInParent<Playe2D2>();
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        player.grounded = true;
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        player.grounded = false;
+    }
+}
---
+++ ComunasFighters/Assets/Scripts/MenuController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuController : MonoBehaviour
+{
+    public Canvas CanvasObject;
+
+    public void playGame()
+    {
+        //SceneManager.LoadScene("SampleScene");
+        CanvasObject.enabled = false;
+    }
+    void Start()
+    {
+        CanvasObject = GetComponent<Canvas>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void exitGame()
+    {
+        Application.Quit();
+    }
+}
